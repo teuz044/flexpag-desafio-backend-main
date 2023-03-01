8ad582cb981044eefef72209a4595371323446d6
Matheus Gabriel <103612213+teuz044@users.noreply.github.com>
2023-03-01 18:23:46 -0300

    Estrutura REST adicionada

---
+++ src/main/java/com/flexpag/paymentscheduler/model/SchedulerModel.java
@@ -0,0 +1,6 @@
+package com.flexpag.paymentscheduler.model;
+
+public class SchedulerModel {
+
+
+}
---
+++ src/main/java/com/flexpag/paymentscheduler/repository/SchedulerRepository.java
@@ -0,0 +1,6 @@
+package com.flexpag.paymentscheduler.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SchedulerRepository extends JpaRepository<> {
+}
