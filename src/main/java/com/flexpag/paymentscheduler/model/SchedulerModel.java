f92a6690d5bab327c769d40a95827a3dea0eff5c
Matheus Gabriel <103612213+teuz044@users.noreply.github.com>
2023-03-05 03:27:20 -0300

    Anotações adicionadas para configuração do Swagger

--- src/main/java/com/flexpag/paymentscheduler/model/SchedulerModel.java
+++ src/main/java/com/flexpag/paymentscheduler/model/SchedulerModel.java
@@ -1,28 +1,51 @@
 package com.flexpag.paymentscheduler.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.UUID;
 
 @Entity
 @Table(name = "scheduler")
+@JsonIgnoreProperties(ignoreUnknown = true)
+@ApiModel(description = "Modelo para agendamento de pagamento")
 public class SchedulerModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     @Column(name = "id")
+    @JsonIgnore
     private Integer id;
+
+
+    @NotNull
     @Column(name = "valorPagamento", nullable = false)
     private Long valorPagamento;
+
+    @NotBlank
     @Column(name = "dataVencimento", nullable = false)
     private LocalDate dataVencimento;
+
+    @JsonIgnore
     @Column(name = "dataAgendamento", nullable = false)
     private LocalDate dataAgendamento = LocalDate.now();
+
+    @JsonIgnore
     @Column(name = "horaAgendamento", nullable = false)
     private LocalTime horaAgendamento = LocalTime.now();
+
+    @JsonIgnore
     @Enumerated(EnumType.STRING)
     @Column(name = "status")
     private StatusPagamento status = StatusPagamento.PENDING;
+
+    @NotBlank
     @Column(name = "beneficiario", nullable = false)
     private String beneficiario;
 
